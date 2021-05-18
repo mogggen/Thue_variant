92d9bb6fb2001c251919c7f6f48a1ffa2af28a97
Morgan Nyman <57505931+mogggen@users.noreply.github.com>
2021-05-19 00:58:52 +0200

    initial commit

---
+++ variant_interpreter.py
@@ -0,0 +1,24 @@
+def run(path):
+    f = open(path, 'r')
+    h = f.readlines()
+    init = h.pop().strip("\n")
+
+    if init.isalnum():
+        while h:
+            i = h.pop(0).strip("\n")
+            if i == "::=":
+                break
+            if ":::=" in i:
+                left = i.split(":::=")[0]
+                right = input()
+            else:
+                left, right = i.split("::=")
+                if right[:1] == '~':
+                    right = right[1:]
+                    for left in init:
+                        print(right, end="")
+            init = init.replace(left, right)
+    f.close()
+
+
+run("input.txt")
